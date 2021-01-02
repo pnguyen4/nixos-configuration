249e966bea91ff76e8c512f7f9bdab3b6af50f9c
Phillip Nguyen <pnguyen4711@gmail.com>
2021-01-02 12:01:15 -0500

    save config from test run

---
+++ configuration.nix
@@ -0,0 +1,155 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchGit {
+    url = "https://github.com/nix-community/home-manager.git";
+    rev = "22f6736e628958f05222ddaadd7df7818fe8f59d";
+    ref = "release-20.09";
+  };
+in
+{
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      # Home-Manager Module
+      (import "${home-manager}/nixos")
+    ];
+
+  # GPU Passthrough for VMs
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [ "amd_iommu=on" "pcie_aspm=off" "vfio-pci.ids=1002:687f,1002:aaf8" ];
+  boot.kernelModules = [ "kvm-amd" "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
+  boot.blacklistedKernelModules = [ "amdgpu" ];
+  boot.initrd.kernelModules = [ "vfio_pci" "amdgpu" ];
+  boot.extraModprobeConfig = ''
+    softdep amdgpu pre: vfio vfio-pci
+    options vfio-pci ids=1002:687f,1002:aaf8
+  '';
+  boot.initrd.preDeviceCommands = ''
+    DEVS="0000:0a:00.0 0000:0a:00.1"
+    for DEV in $DEVS; do
+      echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
+    done
+    modprobe -i vfio-pci
+  '';
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemuOvmf = true;
+    qemuRunAsRoot = false;
+    onBoot = "ignore";
+    onShutdown = "shutdown";
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.device = "/dev/disk/by-id/wwn-0x5000c50093411ab1";
+
+  networking.hostName = "nixos-machine"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp4s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+  #   font = "Lat2-Terminus16";
+      keyMap = "dvorak";
+  };
+
+  # Configure X Server and Window Manager
+  services.xserver = {
+      enable = true;
+      displayManager.lightdm.enable = true;
+      windowManager.i3 = {
+          enable = true;
+          extraPackages = with pkgs; [
+            dmenu
+            i3status
+            i3lock
+          ];
+          extraSessionCommands = "${pkgs.autorandr}/bin/autorandr -c";
+      };
+  };
+
+  # Configure AMD video drivers
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  services.acpid.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.layout = "dvorak";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.user = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "libvirtd"]; # Enable ‘sudo’ for the user.
+  };
+  home-manager.users.user = import /home/user/home.nix;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+      wget vim htop pciutils git virtmanager
+  ];
+
+  fonts.fonts = with pkgs; [
+      terminus_font
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+}
