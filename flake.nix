79c46ccb714092b16f2d9ccf3eb40b1c317406fb
Phillip Nguyen <pnguyen4711@gmail.com>
2022-12-04 15:07:09 -0600

    fix home-manager to follow stable release

--- flake.nix
+++ flake.nix
@@ -3,11 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixos-hardware.url = "github:NixOS/nixos-hardware";
     nur = {
       url = "github:nix-community/NUR";
     };
